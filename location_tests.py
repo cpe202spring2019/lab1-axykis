bc347a6cf1e06fcad8dd5d66dfe84a6fa1c8cf1d
alexanderxykis <alexanderxykis@gmail.com>
2019-04-12 10:05:36 -0700

    NoerrorDayof

--- location_tests.py
+++ location_tests.py
@@ -11,12 +11,22 @@ class TestLab1(unittest.TestCase):
         loc = Location("SLO", 35.3, -120.7)
         self.assertNotEqual(repr(loc), "NOTSLO, 35.3, -120.7")
 
-    def test_repr(self):
+    def test_repr_two_equal_objects(self):
         loc = Location("SLO", 35.3, -120.7)
-        self.assertEqual(repr(loc), "SLO, 35.3, -120.7")
+        loc2 = Location("SLO", 35.3, -120.7)
+        self.assertEqual(repr(loc), repr(loc2))
 
+    def test_repr_two_different_objects(self):
+        loc = Location("SLO", 35.3, -120.7)
+        loc2 = Location("San Diego", 32.7, -117.2)
+        self.assertNotEqual(repr(loc), repr(loc2))
 
-    # Add more tests!
+    def test_repr_setting_two_objects_equal(self):
+        loc = Location("SLO", 35.3, -120.7)
+        loc2 = loc
+        self.assertEqual(repr(loc), repr(loc2))
+
+    
     def test_eq(self):
         loc = Location("San Diego", 32.7, -117.2)
         loc2 = Location("San Diego", 32.7, -117.2)
@@ -25,9 +35,14 @@ class TestLab1(unittest.TestCase):
     def test_eq_set_obj_equal(self):
         loc1 = Location("SLO", 35.3, -120.7)
         loc2=loc1
-        #self.assertEqual()
-'''assert true
-assert false
-notequal'''
+        self.assertEqual(loc1,loc2)
+
+    def test_eq_set_3obj_equal(self):
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = loc1
+        loc3 = loc2
+        self.assertEqual(loc1,loc2,loc3)
+
+
 if __name__ == "__main__":
         unittest.main()
